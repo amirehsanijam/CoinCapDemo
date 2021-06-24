6e06322c25e6930de7a689bb143bc2e7b803ebd0
amirehsanijam <amir.ehsani.jam@gmail.com>
2021-06-24 17:30:16 +0430

    Implemented CryptoDetail (#5)

--- CoinCapDemo/CoinCapDemo/AppModules/CyptoDetail/CryptoDetailViewController.swift
+++ CoinCapDemo/CoinCapDemo/AppModules/CyptoDetail/CryptoDetailViewController.swift
@@ -5,21 +5,63 @@
 //  Created by Amir on 6/23/21.
 //
 
-import UIKit
+import Kingfisher
 import RxSwift
 import RxCocoa
+import UIKit
 
 class CryptoDetailViewController: BaseViewController<DefaultViewState, CryptoDetailViewModel> {
+  @IBOutlet weak private var name: UILabel!
+  @IBOutlet weak private var cryptoDescription: UILabel!
+  @IBOutlet weak private var logo: UIImageView!
 
   override func setupView() {
     super.setupView()
+
+    logo.layer.cornerRadius = logo.frame.height / 2
   }
 
   override func setupBindings() {
     super.setupBindings()
+
+    viewModel
+      .dataSource
+      .subscribe(
+        onNext: { model in
+          self.updateUI(model: model)
+        }
+      ).disposed(by: bag)
+
+    viewModel.getData()
+  }
+
+  private func updateUI(model: CryptoDetailResponseModel) {
+    let logoURL = URL(string: model.logo ?? "")
+    logo.kf.setImage(with: logoURL)
+    name.text = model.name
+    cryptoDescription.text = model.datumDescription
   }
 
   override func stateDidChanged(state: DefaultViewState) {
     super.stateDidChanged(state: state)
+
+    switch state {
+    case .loading:
+      loading(true)
+    case .error(let error):
+      showMessage(message: error.localizedDescription)
+    case .idle:
+      loading(false)
+    case .success:
+      loading(false)
+    }
+  }
+
+  override func loading(_ enabled: Bool) {
+    super.loading(enabled)
+
+    logo.isHidden = enabled
+    name.isHidden = enabled
+    cryptoDescription.isHidden = enabled
   }
 }
